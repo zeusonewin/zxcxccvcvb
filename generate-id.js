6e05728fa1cc9e7d6afd5bd218bb4702fcaa38cc
zeusonewin <zeusonewin@gmail.com>
2026-02-13 01:08:24 +0300

    Delete generate-id.js

--- generate-id.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const { userId } = req.query;
-  if (!userId) return res.status(400).json({ error: "No userId" });
-
-  const personalId = ((parseInt(userId) % 900000) + 100000).toString();
-  res.status(200).json({ personalId });
-}
